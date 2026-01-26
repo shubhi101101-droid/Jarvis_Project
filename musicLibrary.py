9f7bfba89abe487c245e64346e276d893bc57ddd
Subham Arya <shubhi101101@gmail.com>
2026-01-26 16:04:16 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "aujla": "https://youtu.be/XTp5jaRU3Ws?si=OqUlNmNShW6QOqhz",
+    "oo janna": "hhttps://youtu.be/0pehqPZuB8A?si=_duKBMZtSo0fMMVp",
+    "maruti": "https://youtu.be/_ijaEtNzZgw?si=HeT4zMZRNpP8H1W8"
+
+}
